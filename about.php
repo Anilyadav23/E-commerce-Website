135a2745fc19bda830945e522ad1bacabc271c0a
Anilyadav23 <67097755+Anilyadav23@users.noreply.github.com>
2023-04-17 22:59:05 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,80 @@
+<?php
+    include './includes/common.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include './includes/links.php'; ?>
+    <title>About | Mobile Shoppee</title>
+</head>
+<body>
+    <!-- Header -->
+    <?php
+            include './includes/header.php';
+        ?>
+    <!-- Header End -->
+
+    <!-- Main -->
+        <div class="content">
+            <div class="container">
+              
+            <div class="row">
+                <div class="col-md-4">
+                  <h3 class="about-h3 text-uppercase">Who We Are</h3>                  
+                  <div class="box-padding-10">
+                  <img src="./img/who-we-are.png" alt="" class="img-responsive img-circle about-img">  
+                  	<h1 class="text-muted about-p">Developer's</h1>   
+            	 	<h3 class="text-muted about-p">Anil Kumar Yadav</h3><p>Web Developer</p>
+  			<h3 class="text-muted about-p">Akash Gupta</h3><p>Web designer</p>
+			<h3 class="text-muted about-p">Anuj Kumar</h3><p>Content Writer</p>
+                  </div>
+                </div>
+
+                <div class="col-md-4">
+                  <h3 class="about-h3">OUR HISTORY</h3>
+                  <div class="box-padding-10">
+                  
+                  <p class="p-bold p-blue">2021 -</p>
+                  <p class="text-muted">We have created an ecommerce model that accompanies that the successful
+completion of any task would be incomplete without
+the mention of people whose ceaseless cooperation
+made it possible, whose constant guidance and
+encouragement crown all efforts with success.
+We also thank our colleagues who have helped in
+successful completion of the project.
+</p>
+<h3>Mrs.Sonika Jindal </h3>
+                </div>
+                </div>
+
+                <div class="col-md-4">
+                  <h3 class="about-h3">OPPURTUNITIES</h3>
+                  <div class="box-padding-10">
+                    <p class="p-bold">Availabe Roles</p>
+                    <div class="box-padding-left">
+                      <p class="text-muted">
+                        1. Jr./Sr. Web Developer<br>
+                        2. Graphic Designer<br>
+                        3. Web Designer<br>
+                        4. Front End Developer<br>
+                        5. Back End Developer<br>
+                        6. Database Admin<br>
+                        7 .System Admin<br>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+    <!-- Main End -->
+
+    <!-- Footer -->
+    <?php
+        include './includes/footer.php';
+    ?>
+    <!-- Footer End -->
+</body>
+</html>
