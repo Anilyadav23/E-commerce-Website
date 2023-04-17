1dcc6985bddb9a075b85d69d62946ddbb8b3c838
Anilyadav23 <67097755+Anilyadav23@users.noreply.github.com>
2023-04-17 23:03:52 +0530

    Add files via upload

--- login_script.php
+++ login_script.php
@@ -1,5 +1,5 @@
 <?php
-    require './includes/common.php';
+    require 'common.php';
 
     $email = $_POST['email'];
     $email = mysqli_real_escape_string($con , $email);
@@ -8,14 +8,14 @@
     $password = mysqli_real_escape_string($con , $password);
     $password = md5($password);
 
-    $login_select_query = "SELECT id, email from users WHERE email = '$email' AND password = '$password'";
+    $login_select_query = "SELECT uid , email from users WHERE email = '$email' AND password = '$password'";
     $login_select_query_result = mysqli_query($con , $login_select_query) or die(mysqli_error($con));
 
     $total_rows_fetched = mysqli_num_rows($login_select_query_result);
     if($total_rows_fetched!=0){
         $row = mysqli_fetch_array($login_select_query_result);
         $_SESSION['email'] = $email;
-        $_SESSION['user_id']=$row['id'];
+        $_SESSION['user_id']=$row['uid'];
         header("location:home.php");
     }
     else{
